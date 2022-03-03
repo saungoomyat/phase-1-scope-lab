4d9fbe6fd5de268e755d8a010350eaf633deed2e
root <saung.myat@gmail.com>
2022-03-02 23:48:50 -0500

    commit tree de8379f24cc30c4548047a86f1109764f2cd0ebc

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    
+         customerName = customerName.toUpperCase();
+         return customerName;
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+//bestCustomer;
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+//bestCustomer;
+
+const leastFavoriteCustomer = 'Saung';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob';
+}
+
+changeLeastFavoriteCustomer();
